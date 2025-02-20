38020996aee711d8131f173fd4f9641b212dac5d
nazaryusypovych <mega.bismen@gmail.com>
2025-02-20 21:28:51 +0200

    Додав Lesson 1

---
+++ Lesson 1/main.js
@@ -0,0 +1,48 @@
+const str = `nazar`;
+const num = 5;
+const boo = true;
+
+const a = undefined; // для не присвоєних значень – автономний тип, який має єдине значення undefined.
+const b = null; // для невідомих значень – автономний тип, який має єдине значення null.
+
+// const obj ={
+//     id: 1,
+//     name: 'nazar'
+// }
+
+const obj2 = {
+
+}
+console.log(j);
+var j = `gfhfbr`; // Глобальна змінна
+let h = `fmgktrmh`; // Блочна змінна
+
+//console.log(j);
+
+
+let user = null;
+
+const obj ={
+    id: 1,
+    name: 'nazar',
+    age:45,
+    "get": false
+}
+
+console.log(obj.name);
+obj.try = `gfhfj`;
+
+delete obj.id; // видалення ключа із обєкту
+console.log(obj);
+
+console.log(obj.hasOwnProperty("tbtrbtrb")); // перевірка наявність ключа в обєкті
+
+let u = `frvrrv`
+
+
+for (const key in obj) { // for in - ітерація обєктів, for of - ітерація масивів
+    console.log(obj[key])
+}
+
+
+// Попрактикуватися із змінними, а також for in, for of
