dbccbeabdde0f97613f8d99802ae33617a3424a6
nazaryusypovych <mega.bismen@gmail.com>
2025-03-13 13:15:06 +0200

    Повторення Homework 2

--- Lesson 2/Homework/main.js
+++ Lesson 2/Homework/main.js
@@ -2,6 +2,7 @@
 // Створити дві функції: переша - створює і повертає масив, друга - приймає результат першої функції і виводить її в консоль.
 
 
+// Спосіб 1:
 
 // створює новий масив
 function craftArray(array){
@@ -17,6 +18,19 @@ function getArray(array) {
     console.log(array);
 
 }
+
+// Спосіб 2:
+
+function CraftObject (name, age, status){ // Створює обєкти не масив
+    this.name = name;
+    this.age = age;
+    this.status = status;
+
+}
+
+let user1 = new CraftObject(`okpk`, 8, true);
+console.log(user1);
+
 
 //----------------------------------------
 
@@ -45,6 +59,35 @@ for (let i = 0; i < array.length; i++) {
     console.log(element)
 
 }
+
+let array2 = [`dnvfvn`, 2, `dnvjevn`, 8, 9];
+
+console.log(array2);
+
+console.log(array2[0]);
+console.log(array2[2]);
+
+
+console.log(array2.length);
+
+array2[5] = 55555;
+console.log(array2);
+
+array2[array2.length] = 666666;
+console.log(array2);
+
+
+for (const array2Element of array2) {
+    console.log(array2Element);
+}
+
+
+for (let i = 0; i < array2.length; i++) {
+    console.log(array2[i]);
+}
+
+
+
 
 
 
@@ -83,5 +126,40 @@ for (const key in user) {
     console.log(`${key} - ${user[key]}`);
 
 }
+
+
+
+
+let user2 = { // обєкт
+    name: 'vova',
+    age: 98,
+    status: false
+}
+
+
+console.log(user2);
+
+console.log(user2.name);
+
+user2.id = 7;
+console.log(user2);
+
+
+for (const user2Key in user2) {
+    console.log(user2Key); // вивести ключі
+
+}
+
+
+for (const user2Key in user2) {
+    console.log(user2[user2Key]); // вивести значення
+}
+
+
+for (const user2Key in user2) {
+    console.log(`${user2Key} - ${user2[user2Key]}`); // вивести значення
+}
+
+
 
 
